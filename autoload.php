a6f3d93f9f2fb88e967c32ca5fcd125d7e7feda0
Matthew Batchelder <borkweb@gmail.com>
2026-07-30 21:44:14 -0400

    Freeze transformer spike contracts

--- autoload.php
+++ autoload.php
@@ -2,18 +2,25 @@
 declare(strict_types=1);
 
 /**
- * Dependency-free PSR-4 autoloader: Automattic\SiteBuild\ → src/.
+ * Dependency-free PSR-4 autoloader for project and vendored source.
  *
  * No Composer. CLI loads this via src/bootstrap.php; embedding hosts require
  * this file and construct SiteBuilder.
  */
 spl_autoload_register(static function (string $class): void {
-    $prefix = 'Automattic\\SiteBuild\\';
-    if (!str_starts_with($class, $prefix)) {
+    $prefixes = [
+        'Automattic\\SiteBuild\\' => __DIR__ . '/src/',
+        'Automattic\\BlocksEngine\\PhpTransformer\\' => __DIR__ . '/lib/blocks-engine-php-transformer/src/',
+    ];
+
+    foreach ($prefixes as $prefix => $baseDir) {
+        if (!str_starts_with($class, $prefix)) {
+            continue;
+        }
+        $file = $baseDir . str_replace('\\', '/', substr($class, strlen($prefix))) . '.php';
+        if (is_file($file)) {
+            require $file;
+        }
         return;
-    }
-    $file = __DIR__ . '/src/' . str_replace('\\', '/', substr($class, strlen($prefix))) . '.php';
-    if (is_file($file)) {
-        require $file;
     }
 });
